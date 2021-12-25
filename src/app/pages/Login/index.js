8fbb362dfd05c393de4e238ecd13c47c0217dff1
MaryAnn <annychuks07@gmail.com>
2021-12-25 12:38:52 +0100

    feat:implement Login & Register page features

--- src/app/pages/Login/index.js
+++ src/app/pages/Login/index.js
@@ -1,5 +1,34 @@
-import React from 'react';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import AppForm from '../../components/Form';
+import { LoginSchema } from '../../utils/FormSchema';
+import { RegisterValues } from '../../utils/FormValues';
+import AuthContainer from '../../shared/AuthContainer';
+import { handleLogIn } from '../../query';
 
-export default function index() {
-  return <div>Login</div>;
+export default function Index() {
+  const [message, setMessage] = useState(null);
+
+  return (
+    <AuthContainer
+      Title={() => <>Login to continue</>}
+      Footer={() => (
+        <>
+          Don't have an account? <Link to="/register">Register now</Link>
+        </>
+      )}
+      message={message}
+    >
+      <AppForm
+        inputList={['email', 'password']}
+        userDetails={RegisterValues}
+        detailSchema={LoginSchema}
+        handleSubmit={(values, resetForm) =>
+          handleLogIn(values, setMessage, resetForm)
+        }
+        submitBtnLabel={'Login'}
+        icons
+      />
+    </AuthContainer>
+  );
 }
--- src/app/pages/Register/index.js
+++ src/app/pages/Register/index.js
@@ -1,5 +1,28 @@
-import React from 'react';
+import { Link } from 'react-router-dom';
+import { addUser } from '../../query';
+import AppForm from '../../components/Form';
+import { RegisterSchema } from '../../utils/FormSchema';
+import { RegisterValues } from '../../utils/FormValues';
+import AuthContainer from '../../shared/AuthContainer';
 
-export default function index() {
-  return <div>Register</div>;
+export default function Index() {
+  return (
+    <AuthContainer
+      Title={() => <>Register to get started</>}
+      Footer={() => (
+        <>
+          Already have an account? <Link to="/login">Login</Link>
+        </>
+      )}
+    >
+      <AppForm
+        inputList={['firstName', 'lastName', 'email', 'password']}
+        userDetails={RegisterValues}
+        detailSchema={RegisterSchema}
+        handleSubmit={(values, resetForm) => addUser(values, resetForm)}
+        submitBtnLabel={'Register'}
+        icons
+      />
+    </AuthContainer>
+  );
 }
